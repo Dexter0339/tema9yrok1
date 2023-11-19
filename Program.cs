d6a1940bfb0bf56fa54508ffa130a8c5e4cb44cf
Dexter0339 <136196150+Dexter0339@users.noreply.github.com>
2023-11-19 17:43:03 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿//1.Операция логического умножения: 
+Console.WriteLine("Операция логического умножения");
+Console.Write("Введите первое число: ");
+int x1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int y1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine(x1 & y1);
+//2.Операция логического сложения
+Console.WriteLine("Операция логического сложения");
+Console.Write("Введите первое число: ");
+int a1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int b1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine(a1 | b1);
+//3.Операция логического исключения
+Console.WriteLine("Операция логического исключения");
+Console.Write("Введите число: ");
+int z = int.Parse(Console.ReadLine()!);
+Console.Write("Введите ключ: ");
+int key = int.Parse(Console.ReadLine()!);
+Console.WriteLine(z ^ key);
